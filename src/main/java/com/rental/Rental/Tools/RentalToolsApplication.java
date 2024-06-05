2c3cec2f83e61b7d2b2546b06b589341af5ad281
Arshad Mohammed <talk2arshads@gmail.com>
2024-06-05 01:58:47 -0400

    project implementation

---
+++ src/main/java/com/rental/Rental/Tools/RentalToolsApplication.java
@@ -0,0 +1,13 @@
+package com.rental.Rental.Tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rental/Rental/Tools/RentalToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.Rental.Tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
